c8ebe0705927f8257dabe919a523798668f45c85
Vedant <vedant.gandhewar@gmail.com>
2020-08-29 09:22:23 -0400

    Updated family portal

--- familyConsole/views.py
+++ familyConsole/views.py
@@ -29,3 +29,28 @@ def activities(request):
     return render(request, 'console/activities.html', {
         'activity_list': activity_list
     })
+
+
+def vital_hub(request):
+    metric_models = [BloodSugarEntry, BloodPresureEntry, PulseEntry, TemperatureEntry, WeightEntry]
+    daily_metrics = []
+    patientId = 1
+    for model in metric_models:
+        try:
+            daily_metrics.append(model.objects.filter(patient=patientId).latest('created_at'))
+        except:
+            pass
+
+    current_prescriptions = Medication.objects.filter(patient=patientId)
+
+    return render(request, 'console/vital_hub.html', {
+        'metrics': daily_metrics,
+        'current_prescriptions': current_prescriptions
+    })
+
+def wellbeing(request):
+    patient = 1
+    activity_list = ActivityEntry.objects.filter(patient=patient)
+    return render(request, 'console/wellbeing.html', {
+        'activity_list': activity_list
+    })
